77b1809e6ee1c36cb34772971b87d6b8c63f4595
Aliaksandr Sukhobski <suhobski@mail.ru>
2019-07-22 07:24:49 +0300

    ArrayActionTest edited

--- test/com/epam/task1/action/ArrayActionTest.java
+++ test/com/epam/task1/action/ArrayActionTest.java
@@ -1,69 +1,87 @@
 package com.epam.task1.action;
 
-import com.epam.task1.init.GenerateArray;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-import java.util.Arrays;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotSame;
 
 public class ArrayActionTest {
 
-  ArrayAction arrayAction =  new ArrayAction();;
-  GenerateArray generateArray = new GenerateArray();;
-  double[] array;
-  double[] expectedArray;
+  private ArrayAction arrayAction =  new ArrayAction();
 
-  @BeforeClass
-  public void setUp() {
-    array = generateArray.generateDoubleArray(100,-1000,1000);
-    expectedArray = new double[array.length];
-    expectedArray = array.clone();
+  @DataProvider
+  public Object[][] findMinimumData(){
+    return new Object[][]{
+            {new double[]{1, 3, 4, -12, 4, 6, 8, -4, 0, 3}, -12},
+            {new double[]{1,2,3}, 1},
+            {new double[]{-5, -78, 0, -99}, -99}
+    };
+  }
+  @Test(dataProvider = "findMinimumData")
+  public void findMinimumTest(double[] inputArray, double expectedValue) {
+    double actualValue = arrayAction.findMinimum(inputArray);
+    assertNotSame(actualValue,expectedValue);
+  }
+
+  @DataProvider
+  public Object[][] findMaximumData(){
+    return new Object[][]{
+            {new double[]{1, 3, 4, -12, 4, 6, 8, -4, 0, 3}, 8},
+            {new double[]{1,2,3}, 3},
+            {new double[]{-5, -78, 0, -99}, 0}
+    };
+  }
+  @Test(dataProvider = "findMaximumData")
+  public void findMaximumTest(double[] inputArray, double expectedValue ) {
+    double actual = arrayAction.findMaximum(inputArray);
+    assertEquals(actual,expectedValue);
+  }
+
+  @DataProvider
+  public Object[][] replaceItemData(){
+    return new Object[][]{
+            {new double[]{1, 3, 4, -12, 4, 6, 8, -4, 0, 3}, new double[]{1, 3, 4, 0, 4, 6, 8, 0, 0, 3}},
+            {new double[]{1,2,3}, new double[]{1,2,3}},
+            {new double[]{-5, -78, 0, -99}, new double[]{0, 0, 0, 0}}
+    };
+  }
+  @Test(dataProvider = "replaceItemData")
+  public void replaceItemTest(double[] inputArray, double[] expected) {
+    double[] actual = inputArray.clone();
+    arrayAction.replaceItem(actual);
+    assertEquals(actual, expected);
   }
 
 
-  @Test
-  public void findMinimumTest() {
-    double actualMinimum = arrayAction.findMinimum(array);
-    double expectedMinimum = Arrays.stream(expectedArray).min().getAsDouble();
-    assertNotSame(actualMinimum,expectedMinimum);
+  @DataProvider
+  public Object[][] findAverageData(){
+    return new Object[][]{
+            {new double[]{1, 3, 4, -12, 4, 6, 8, -4, 0, 3}, 1.0},
+            {new double[]{1,2,3}, 2.0},
+            {new double[]{-5, -78, 0, -99}, -45}
+    };
   }
 
-  @Test
-  public void findMaximumTest() {
-    double actualMaximum = arrayAction.findMaximum(array);
-    double expectedMaximum = Arrays.stream(array).max().getAsDouble();
-    assertEquals(actualMaximum,expectedMaximum);
+  @Test(dataProvider = "findAverageData")
+  public void findAverageValueTest(double[] inputArray, double expectedValue) {
+    double actualValue = arrayAction.findAverage(inputArray);
+    assertEquals(actualValue,expectedValue);
   }
 
-  @Test
-  public void replaceItemTest() {
-//    double[] actualReplacedArray = array;
-//    arrayAction.replaceItem(actualReplacedArray);
-//    double[] expectedReplacedArray = ;
-//    assertEquals(actualReplacedArray, expectedReplacedArray);
+
+  @DataProvider
+  public Object[][] findSumData(){
+    return new Object[][]{
+            {new double[]{1, 3, 4, -12, 4, 6, 8, -4, 0, 3}, 13},
+            {new double[]{1,2,3}, 6},
+            {new double[]{-5, -78, 0, -99}, -182}
+    };
   }
-
-  @Test
-  public void findAverageValueTest() {
-    double actualAverage = arrayAction.findAverage(array);
-    double expectedAverage = Arrays.stream(expectedArray).average().getAsDouble();
-    assertEquals(actualAverage,expectedAverage,1);
-  }
-
-  @Test
-  public void findSumTest() {
-    double[] actualArray = array.clone();
-    double actualSum = arrayAction.findSum(actualArray);
-    double expectedSum = 0;
-    for (double num :
-            expectedArray) {
-      expectedSum += num;
-    }
-    assertEquals(actualSum,expectedSum);
-
+  @Test(dataProvider = "findSumData")
+  public void findSumTest(double[] inputArray, double expectedSum) {
+    double actual = arrayAction.findSum(inputArray);
+    assertEquals(actual,expectedSum);
   }
 
 }
